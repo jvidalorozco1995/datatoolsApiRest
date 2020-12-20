a3ea2942166b14fc55d309ffebcc3677ba146f70
Jorge V <jorgevidal1995@hotmail.com>
2020-12-20 01:19:23 -0500

    Commit refactor models

--- src/main/java/com/org/data/Data1Application.java
+++ src/main/java/com/org/data/Data1Application.java
@@ -1,14 +1,39 @@
 package com.org.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(scanBasePackages={
 "com.org.data", "com.org.data.services"})
-public class Data1Application {
+public class Data1Application extends SpringBootServletInitializer {
 
+	@Autowired
+    private Environment env;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Data1Application.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                String urls = env.getProperty("cors.urls");
+                CorsRegistration reg = registry.addMapping("/api/**");
+                for(String url: urls.split(",")) {
+                    reg.allowedOrigins(url);
+                }
+            }
+        };
+    }  
 
 }
--- src/main/java/com/org/data/controllers/EmpresasController.java
+++ src/main/java/com/org/data/controllers/EmpresasController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.org.data.services.IEmpresasService;
 
 @RestController()
 @RequestMapping("/api/empresas")
+@CrossOrigin
 public class EmpresasController {
 
 	@Autowired
